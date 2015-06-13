ada8dde3236b34736177b00610d3363cbc66c203
Richard Alex Hofer <atomic.quark@gmail.com>
2015-06-13 18:31:06 -0400

    Add initial support for onEnvInit

--- src/RLEnvironment.hs
+++ src/RLEnvironment.hs
@@ -15,7 +15,7 @@ import Paths_rlglue_haskell_codec (version)
 import RLNetwork
 
 data Environment a = Environment
-  { onEnvInit :: (a -> a)
+  { onEnvInit :: (StateT a IO ())
   }
 
 loadEnvironment ::  Environment a -> a -> IO ()
@@ -29,18 +29,20 @@ loadEnvironment env initState =
         sendLazy sock bs
 
         -- Run event loop
-        evalStateT (eventLoop sock) initState
+        evalStateT (eventLoop env sock) initState
   in
     glueConnect func
 
-eventLoop :: Socket -> StateT a IO ()
-eventLoop sock = do
+eventLoop :: Environment a -> Socket -> StateT a IO ()
+eventLoop env sock = do
   x <- lift $ runMaybeT (getEnvState sock)
   case x of
     Nothing -> do
       lift $ putStrLn "Error: Failed to receive state."
       lift $ exitWith (ExitFailure 1)
     Just (state, size) -> case state of
+      kEnvInit -> onEnvInit env
+      kRLTerm -> lift $ return ()
       _ -> do
         lift $ putStrLn $ "Error: Unknown state: " ++ (show state)
         lift $ exitWith (ExitFailure 1)
--- src/SkeletonEnvironment.hs
+++ src/SkeletonEnvironment.hs
@@ -1,8 +1,14 @@
 module Main where
 
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.State.Lazy
+
 import RLEnvironment
 import RLNetwork
 
 main = do
-  loadEnvironment (Environment id) ()
+  loadEnvironment (Environment onInit) ()
   return ()
+
+onInit :: StateT () IO ()
+onInit = lift $ putStrLn "Initialized"
